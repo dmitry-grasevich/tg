417ad7fd9b9e6a691d07dda6d9036ab57c77286d
dmitry-grasevich <dmitry.grasevitch@gmail.com>
2014-10-03 23:20:58 +0300

    small fixes

--- frontend/web/js/wpt.builder.js
+++ frontend/web/js/wpt.builder.js
@@ -5,26 +5,18 @@ function makeDraggable(sortable_id) {
             helper: function () {
                 var imgSrc = self.find('img').attr('src'),
                     fullSrc = imgSrc.replace('thumbs', 'full');
-                return $('<div class="draggable-li"><img src="' + imgSrc + '" width="294px" data-fullimg="' + fullSrc + '" /></div>');
+                return $('<div class="draggable-li" style="width: 294px;"><img src="' + imgSrc + '" width="294px" data-fullimg="' + fullSrc + '" /></div>');
             },
             revert: 'invalid',
             appendTo: 'body',
             connectToSortable: sortable_id,
             stop: function () {
-                //pageEmpty();
-                //allEmpty();
                 $('.draggable-li').remove();
             },
             start: function () {
             }
         });
     });
-
-    $('#elements li a').each(function () {
-        $(this).unbind('click').bind('click', function (e) {
-            e.preventDefault();
-        })
-    })
 }
 
 function makeSortable(el) {
@@ -33,7 +25,7 @@ function makeSortable(el) {
         placeholder: "drop-hover",
         beforeStop: function (event, ui) {
             var fullSrc = ui.helper.find('img').data('fullimg');
-            ui.item.find('a').html($('<img src="' + fullSrc + '" width="1200px" data-fullimg="' + fullSrc + '" />'));
+            ui.item.find('a').parent().html($('<img src="' + fullSrc + '" width="1200px" data-fullimg="' + fullSrc + '" />'));
         }
         //stop: function () {
         //
