03aba54548289d7ce67c6b1a7901c2696297b884
dmitry-grasevich <dmitry.grasevitch@gmail.com>
2015-07-27 12:59:47 +0300

    #18 updated dir 'backend'

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,12 +1,14 @@
 <?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
 use backend\assets\AppAsset;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
-
-/* @var $this \yii\web\View */
-/* @var $content string */
+use common\widgets\Alert;
 
 AppAsset::register($this);
 ?>
@@ -14,7 +16,7 @@ AppAsset::register($this);
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>"/>
+    <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
