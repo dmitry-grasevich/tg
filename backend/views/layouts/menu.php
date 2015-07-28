4f323d87588cb9757c4393c9be6851ed779dbdee
dmitry-grasevich <dmitry.grasevitch@gmail.com>
2015-07-28 21:56:20 +0300

    various implementations for the admin menu

--- backend/views/layouts/menu.php
+++ backend/views/layouts/menu.php
@@ -1,3 +1,8 @@
+<?php
+use common\models\Category;
+
+$categories = Category::findAll(['is_basic' => 0]);
+?>
 <nav id="mp-menu" class="mp-menu">
     <div class="mp-level">
         <h2>Menu</h2>
@@ -8,7 +13,41 @@
                 </a>
             </li>
             <li class="icon icon-arrow-left">
-                Blocks
+                <a href="#">Blocks</a>
+                <div class="mp-level">
+                    <h2>Blocks</h2>
+                    <a class="mp-back" href="#">back</a>
+                    <ul>
+                        <?php /** @var common\models\Category $category */ ?>
+                        <?php foreach ($categories as $category): ?>
+                            <li class="icon icon-arrow-left">
+                                <a href="#"><?= $category->name ?></a>
+                                <div class="mp-level">
+                                    <h2><?= $category->name ?></h2>
+                                    <a class="mp-back" href="#">back</a>
+                                    <div class="items-list">
+                                        <ul>
+                                            <?php /** @var common\models\Template $template */ ?>
+                                            <?php foreach ($category->templates as $template): ?>
+                                                <li class="menu-item">
+                                                    <a href="#">
+                                                        <?= $template->name ?>
+                                                        <img src="/images/elements/<?= $category->alias ?>/thumbs/<?= $template->img ?>" width="294px" data-id="<?= $template->id ?>" />
+                                                    </a>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php endforeach; ?>
+                        <li class="icon icon-plus">
+                            <a href="#">
+                                Add new block
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </li>
         </ul>
     </div>
