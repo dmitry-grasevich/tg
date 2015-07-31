dcd899270ee3fad79b3c6948e8b6d8995c5efe86
dmitry-grasevich <dmitry.grasevitch@gmail.com>
2015-07-31 17:15:36 +0300

    #22 added buttons to fast switch between view and edit of Template

--- backend/views/template/edit.php
+++ backend/views/template/edit.php
@@ -5,16 +5,14 @@
  * @var common\models\Template $template
  */
 
+use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Button;
 use kartik\widgets\FileInput;
 
-$title = Yii::t('tg', $template->isNewRecord ? 'New Block in category "{category}"' :
-    'Edit Block "{template}" in category "{category}"', [
-    'category' => $category->name,
-    'template' => $template->name,
-]);
+$title = Yii::t('tg', $template->isNewRecord ? 'New block in category "{category}"' :
+    'Edit block in category "{category}"', ['category' => $category->name, 'template' => $template->name]);
 
 $this->title = $title;
 
@@ -22,78 +20,107 @@ $labels = $template->attributeLabels();
 ?>
 
 <div id="page-title">
-    <h1 class="page-header text-overflow"><?= $title ?></h1>
-</div>
-
-<div class="col-md-8 col-md-offset-2">
-    <?php $form = ActiveForm::begin([
-        'id' => 'template-form',
-        'class' => 'form-horizontal',
-        'options' => [
-            'enctype' => 'multipart/form-data'
-        ]
-    ]); ?>
-
-    <div class="checkbox">
-        <label class="form-checkbox form-normal form-success form-text">
-            <?= Html::checkbox('Template[is_visible]', $template->is_visible) . ' ' . $labels['is_visible'] ?>
-        </label>
-    </div>
-
-    <?= $form->field($template, 'name')->textInput([
-        'placeholder' => Yii::t('tg', 'Internal system name (usually equal to Title)'),
-    ]) ?>
-
-    <?= $form->field($template, 'alias')->textInput([
-        'placeholder' => Yii::t('tg', 'lowercase without spaces, wil be used as index in the customizer\'s panels array'),
-    ]) ?>
-
-    <?= $form->field($template, 'title')->textInput([
-        'placeholder' => Yii::t('tg', 'Title of panel in the customizer'),
-    ]) ?>
-
-    <?= $form->field($template, 'description')->textarea([
-        'class' => 'form-control',
-        'rows' => 5,
-        'placeholder' => Yii::t('tg', 'Description of panel in the customizer'),
-    ]) ?>
-
-    <?= $form->field($template, 'img')->widget(FileInput::className(), [
-        'options' => [
-            'accept' => 'image/*',
-        ],
-        'pluginOptions' => [
-            'showUpload' => false,
-            'previewSettings' => [
-                'image' => [
-                    'width' => '100%',
-                    'height' => 'auto',
+    <h1 class="page-header text-overflow">
+        <?= $title ?>
+        <?php if (!$template->isNewRecord) {
+            echo Button::widget([
+                'label' => '<i class="fa fa-file-code-o fa-fw"></i> View',
+                'encodeLabel' => false,
+                'options' => [
+                    'id' => 'view-template-btn',
+                    'class' => 'btn-default'
                 ]
-            ],
-            'initialPreview' => $template->img ? [
-                Html::img($template->getImagePath() . '/' . $template->img, ['class'=>'file-preview-image']),
-            ] : false,
-        ],
-    ]) ?>
-
-    <?= $form->field($template, 'code')->textarea([
-        'class' => 'form-control',
-        'rows' => 10,
-        'placeholder' => Yii::t('tg', 'Block code'),
-    ]) ?>
-
-    <?= $form->field($template, 'id')->hiddenInput()->label(false) ?>
-    <?= Html::hiddenInput('Template[category_id]', $category->id) ?>
-
-    <div class="text-right">
-        <?= Button::widget([
-            'label' => 'SAVE',
-            'options' => [
-                'id' => 'save-template-btn',
-                'class' => 'btn-primary',
-            ]
-        ]) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+            ]);
+        } ?>
+    </h1>
 </div>
+
+<div id="page-content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel">
+                <div class="panel-body">
+                    <?php $form = ActiveForm::begin([
+                        'id' => 'template-form',
+                        'class' => 'form-horizontal',
+                        'options' => [
+                            'enctype' => 'multipart/form-data'
+                        ]
+                    ]); ?>
+
+                    <div class="checkbox">
+                        <label class="form-checkbox form-normal form-success form-text">
+                            <?= Html::checkbox('Template[is_visible]', $template->is_visible) . ' ' . $labels['is_visible'] ?>
+                        </label>
+                    </div>
+
+                    <?= $form->field($template, 'name')->textInput([
+                        'placeholder' => Yii::t('tg', 'Internal system name (usually equal to Title)'),
+                    ]) ?>
+
+                    <?= $form->field($template, 'alias')->textInput([
+                        'placeholder' => Yii::t('tg', 'lowercase without spaces, wil be used as index in the customizer\'s panels array'),
+                    ]) ?>
+
+                    <?= $form->field($template, 'title')->textInput([
+                        'placeholder' => Yii::t('tg', 'Title of panel in the customizer'),
+                    ]) ?>
+
+                    <?= $form->field($template, 'description')->textarea([
+                        'class' => 'form-control',
+                        'rows' => 5,
+                        'placeholder' => Yii::t('tg', 'Description of panel in the customizer'),
+                    ]) ?>
+
+                    <?= $form->field($template, 'img')->widget(FileInput::className(), [
+                        'options' => [
+                            'accept' => 'image/*',
+                        ],
+                        'pluginOptions' => [
+                            'showUpload' => false,
+                            'previewSettings' => [
+                                'image' => [
+                                    'width' => '100%',
+                                    'height' => 'auto',
+                                ]
+                            ],
+                            'initialPreview' => $template->img ? [
+                                Html::img($template->getImagePath() . '/' . $template->img, ['class'=>'file-preview-image']),
+                            ] : false,
+                        ],
+                    ]) ?>
+
+                    <?= $form->field($template, 'code')->textarea([
+                        'class' => 'form-control',
+                        'rows' => 10,
+                        'placeholder' => Yii::t('tg', 'Block code'),
+                    ]) ?>
+
+                    <?= $form->field($template, 'id')->hiddenInput()->label(false) ?>
+                    <?= Html::hiddenInput('Template[category_id]', $category->id) ?>
+
+                    <div class="text-right">
+                        <?= Button::widget([
+                            'label' => 'SAVE',
+                            'options' => [
+                                'id' => 'save-template-btn',
+                                'class' => 'btn-primary',
+                            ]
+                        ]) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+$viewProfileUrl = Url::to(['/template/view', 'id' => $template->id]);
+$js = <<<JS
+$('#view-template-btn').on('click', function() {
+    window.location.href = "{$viewProfileUrl}";
+});
+JS;
+$this->registerJs($js, $this::POS_READY, 'template-edit-script');
--- backend/views/template/view.php
+++ backend/views/template/view.php
@@ -4,6 +4,9 @@
  * @var common\models\Template $template
  */
 
+use yii\helpers\Url;
+use yii\bootstrap\Button;
+
 $title = Yii::t('tg', 'Block "{template}" ({alias})', [
     'template' => $template->name,
     'alias' => $template->alias,
@@ -22,6 +25,17 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-sm-6">
                 <div class="panel">
                     <div class="panel-heading">
+                        <div class="panel-control">
+                            <?= Button::widget([
+                                'label' => '<i class="fa fa-pencil fa-fw"></i> Edit',
+                                'encodeLabel' => false,
+                                'options' => [
+                                    'id' => 'edit-template-btn',
+                                    'class' => 'btn-default'
+                                ]
+                            ]) ?>
+                        </div>
+
                         <h3 class="panel-title"><?= Yii::t('tg', 'Block Preview') ?></h3>
                     </div>
                     <div class="panel-body">
@@ -33,7 +47,7 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="col-sm-3">
                 <div class="panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title"><?= Yii::t('tg', 'Control Params') ?></h3>
+                        <h3 class="panel-title"><?= Yii::t('tg', 'Control Settings') ?></h3>
                     </div>
                     <div class="panel-body">
 
@@ -55,6 +69,7 @@ $this->params['breadcrumbs'][] = $this->title;
     </div>
 
 <?php
+$editProfileUrl = Url::to(['/template/edit', 'cat' => $template->category_id, 'id' => $template->id]);
 $js = <<<JS
 
 hljs.initHighlightingOnLoad();
@@ -63,6 +78,9 @@ $('pre.scroll').perfectScrollbar({ suppressScrollX: true });
 nifty.container.hasClass("aside-in") ? ($.niftyAside("hide")) : ($.niftyAside("show"));
 $.niftyAside("fixedPosition");
 
+$('#edit-template-btn').on('click', function() {
+    window.location.href = "{$editProfileUrl}";
+});
 JS;
 $this->registerJs($js, $this::POS_READY, 'template-view-script');
 
